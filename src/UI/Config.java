57ad6a279b4a4826113f23234ca6d6da3b948896
antonsemenov8703 <102480900+antonsemenov8703@users.noreply.github.com>
2023-03-18 10:35:50 +0530

    Initial commit

---
+++ src/UI/Config.java
@@ -0,0 +1,5 @@
+package UI;
+
+public class Config {
+    public static String pathDb = "data.db";
+}
---
+++ src/UI/NewConsoleView.java
@@ -0,0 +1,15 @@
+package UI;
+
+public class NewConsoleView extends ConsoleView {
+
+    public NewConsoleView() {
+        super();
+    }
+
+    @Override
+    public void setDescription(String value) {
+        super.setDescription(value);
+        System.out.println();
+    }
+
+}
